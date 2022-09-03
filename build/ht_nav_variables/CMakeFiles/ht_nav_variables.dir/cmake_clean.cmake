bcd03c812047b092e9b55bd2e503308435ddf7cc
Halil Temurtaş <halil.temurtas@metu.edu.tr>
2022-09-03 15:19:03 +0300

    Delete /build/ folder from repo

--- build/ht_nav_variables/CMakeFiles/ht_nav_variables.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ht_nav_variables"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ht_nav_variables.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
